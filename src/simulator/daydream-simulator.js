fd64deb861d6e1cad005cf5bddf8585fa200b815
Boris Smus <boris@smus.com>
2016-10-11 10:33:02 -0700

    add license

--- src/simulator/daydream-simulator.js
+++ src/simulator/daydream-simulator.js
@@ -1,10 +1,24 @@
-/**
- * Responsible for rendering a stick figure with the arm model from a third
- * person view. Takes in a calculated pose of the controller.
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 const FIGURE_HEIGHT = 1.6;
 
+/**
+ * Responsible for rendering a stick figure with the arm model from a third
+ * person view. Takes in a calculated pose of the controller.
+ */
 export default class DaydreamSimulator {
 
   constructor(model) {
